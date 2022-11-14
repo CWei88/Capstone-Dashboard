4f860cab92ff23095cc677a74dd3859595b23553
An Khanh <e0407674@u.nus.edu>
2022-11-14 16:51:53 +0800

    Combine Yearly and Industry Summary

--- frontend/src/components/DataSummary.js
+++ frontend/src/components/DataSummary.js
@@ -1,89 +1,56 @@
-import YearSummary from "./YearSummary";
-import IndustrySummary from "./IndustrySummary";
+import SummaryChart from "./SummaryChart";
 
 export default function DataSummary({Reports}) {
    console.log("All Reports from IndustrySummary");
-   // Filter for reports in 2019, 2020, 2021, 2022, 2023
-   // Summary Reports by Year
+   // Summary Reports by Year and Industry
    let yearList = [...new Set(Reports.map(item => item.year))]; 
-   let summaryByYear = [];
-   for (var i = 0; i < yearList.length; i++) { 
-      var currentYear = yearList[i];
-      console.log(currentYear); 
-      var reportByYear = Reports.filter((report) => {
-         return report.year === currentYear;
-      });
-      console.log(currentYear); 
-      console.log(reportByYear);
-      var numReport = reportByYear.length; 
-      var numEmissionAmountVerified = reportByYear.reduce((sum, { emissionAmountVerified }) => sum + emissionAmountVerified, 0);
-      var numHasActiveProgram = reportByYear.reduce((sum, { hasActiveProgram }) => sum + hasActiveProgram, 0);
-      var numHasNetZeroGoal = reportByYear.reduce((sum, { hasNetZeroGoal }) => sum + hasNetZeroGoal, 0);
-      var numHasProtocol = reportByYear.reduce((sum, { hasProtocol }) => sum + hasProtocol, 0);
-      var numHasEmissionTargetVerified = reportByYear.reduce((sum, { emissionTargetVerified }) => sum + emissionTargetVerified, 0);
-      var numHasTransitionPlan = reportByYear.reduce((sum, { hasTransitionPlan }) => sum + hasTransitionPlan, 0);
-      var numHasLeadershipIncentive = reportByYear.reduce((sum, { hasLeadershipIncentive }) => sum + hasLeadershipIncentive, 0);
-      var numHasSupplierEngagement = reportByYear.reduce((sum, { hasSupplierEngagement }) => sum + hasSupplierEngagement, 0);
-      var numHasValueChainEngagement = reportByYear.reduce((sum, { hasValueChainEngagement }) => sum + hasValueChainEngagement, 0);
-      summaryByYear.push({
-         year: currentYear, 
-         numReport: numReport,
-         numEmissionAmountVerified: numEmissionAmountVerified,
-         numHasActiveProgram: numHasActiveProgram,
-         numHasNetZeroGoal: numHasNetZeroGoal,
-         numHasProtocol: numHasProtocol,
-         numHasEmissionTargetVerified: numHasEmissionTargetVerified,
-         numHasTransitionPlan: numHasTransitionPlan,
-         numHasLeadershipIncentive: numHasLeadershipIncentive,
-         numHasSupplierEngagement: numHasSupplierEngagement,
-         numHasValueChainEngagement: numHasValueChainEngagement
-      })
-   }
-   console.log("SummaryByYear Data Summary: ");
-   console.log(summaryByYear);
-
-   // Summary Report by Industry
    let industryList = [...new Set(Reports.map(item => item.industry))];
-   let summaryByIndustry = [];
-   for (i = 0; i < industryList.length; i++) { 
-      var currentIndustry = industryList[i];
-      console.log(currentIndustry); 
-      var reportByIndustry = Reports.filter((report) => {
-         return report.industry === currentIndustry;
-      });
-      console.log(currentIndustry); 
-      console.log(reportByIndustry);
-      numReport = reportByIndustry.length; 
-      numEmissionAmountVerified = reportByIndustry.reduce((sum, { emissionAmountVerified }) => sum + emissionAmountVerified, 0);
-      numHasActiveProgram = reportByIndustry.reduce((sum, { hasActiveProgram }) => sum + hasActiveProgram, 0);
-      numHasNetZeroGoal = reportByIndustry.reduce((sum, { hasNetZeroGoal }) => sum + hasNetZeroGoal, 0);
-      numHasProtocol = reportByIndustry.reduce((sum, { hasProtocol }) => sum + hasProtocol, 0);
-      numHasEmissionTargetVerified = reportByIndustry.reduce((sum, { emissionTargetVerified }) => sum + emissionTargetVerified, 0);
-      numHasTransitionPlan = reportByIndustry.reduce((sum, { hasTransitionPlan }) => sum + hasTransitionPlan, 0);
-      numHasLeadershipIncentive = reportByIndustry.reduce((sum, { hasLeadershipIncentive }) => sum + hasLeadershipIncentive, 0);
-      numHasSupplierEngagement = reportByIndustry.reduce((sum, { hasSupplierEngagement }) => sum + hasSupplierEngagement, 0);
-      numHasValueChainEngagement = reportByIndustry.reduce((sum, { hasValueChainEngagement }) => sum + hasValueChainEngagement, 0);
-      summaryByIndustry.push({
-         industry: currentIndustry, 
-         numReport: numReport,
-         numEmissionAmountVerified: numEmissionAmountVerified,
-         numHasActiveProgram: numHasActiveProgram,
-         numHasNetZeroGoal: numHasNetZeroGoal,
-         numHasProtocol: numHasProtocol,
-         numHasEmissionTargetVerified: numHasEmissionTargetVerified,
-         numHasTransitionPlan: numHasTransitionPlan,
-         numHasLeadershipIncentive: numHasLeadershipIncentive,
-         numHasSupplierEngagement: numHasSupplierEngagement,
-         numHasValueChainEngagement: numHasValueChainEngagement
-      })
+   let summaryData = [];
+   for (var i = 0; i < yearList.length; i++) { 
+      for (var k = 0; k < industryList.length; k++) {
+         var currentYear = yearList[i];
+         var currentIndustry = industryList[k];
+         var filteredReports = Reports.filter((report) => {
+            return (report.year === currentYear && report.industry === currentIndustry);
+         });
+         console.log(currentYear); 
+         console.log(currentIndustry);
+         console.log(filteredReports);
+         var numReport = filteredReports.length; 
+         var numEmissionAmountVerified = filteredReports.reduce((sum, { emissionAmountVerified }) => sum + emissionAmountVerified, 0);
+         var numHasActiveProgram = filteredReports.reduce((sum, { hasActiveProgram }) => sum + hasActiveProgram, 0);
+         var numHasNetZeroGoal = filteredReports.reduce((sum, { hasNetZeroGoal }) => sum + hasNetZeroGoal, 0);
+         var numHasProtocol = filteredReports.reduce((sum, { hasProtocol }) => sum + hasProtocol, 0);
+         var numHasEmissionTargetVerified = filteredReports.reduce((sum, { emissionTargetVerified }) => sum + emissionTargetVerified, 0);
+         var numHasTransitionPlan = filteredReports.reduce((sum, { hasTransitionPlan }) => sum + hasTransitionPlan, 0);
+         var numHasLeadershipIncentive = filteredReports.reduce((sum, { hasLeadershipIncentive }) => sum + hasLeadershipIncentive, 0);
+         var numHasSupplierEngagement = filteredReports.reduce((sum, { hasSupplierEngagement }) => sum + hasSupplierEngagement, 0);
+         var numHasValueChainEngagement = filteredReports.reduce((sum, { hasValueChainEngagement }) => sum + hasValueChainEngagement, 0);
+         summaryData.push({
+            year: currentYear, 
+            industry: currentIndustry,
+            numReport: numReport,
+            numEmissionAmountVerified: numEmissionAmountVerified,
+            numHasActiveProgram: numHasActiveProgram,
+            numHasNetZeroGoal: numHasNetZeroGoal,
+            numHasProtocol: numHasProtocol,
+            numHasEmissionTargetVerified: numHasEmissionTargetVerified,
+            numHasTransitionPlan: numHasTransitionPlan,
+            numHasLeadershipIncentive: numHasLeadershipIncentive,
+            numHasSupplierEngagement: numHasSupplierEngagement,
+            numHasValueChainEngagement: numHasValueChainEngagement
+         })
+      }
+
    }
-   console.log("SummaryByIndustry Data Summary: ");
-   console.log(summaryByIndustry);
+   console.log("SummaryData Data Summary: ");
+   console.log(summaryData);
+   console.log(yearList);
+   console.log(industryList);
    return (
       <div>
          <p>Data Summary</p>
-         <YearSummary summaryByYear={summaryByYear} yearList={yearList}/>
-         <IndustrySummary summaryByIndustry={summaryByIndustry} industryList={industryList}/>
+         <SummaryChart summaryData={summaryData} yearList={yearList} industryList={industryList}/>
       </div>
    );
 }
---
+++ frontend/src/components/SummaryChart.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect } from 'react';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+export default function SummaryChart({summaryData, yearList, industryList}) {
+    const [chartData, setChartData] = useState([]);
+    const [numReport, setNumReport] = useState(0);
+    const [year, setYear] = useState("2019");
+    const [industry, setIndustry] = useState("Automobile");
+
+    const handleChange = (event) => {
+        if (event.target.name === "year") {
+            var currentYear = event.target.value;
+            const filteredSummary = summaryData.filter((summary) => {
+                return (summary.year.toString() === currentYear && summary.industry === industry);
+            })[0];
+            console.log(filteredSummary);
+            console.log(currentYear);
+            console.log(industry);
+            setChartData([
+                {name: 'numEmissionAmountVerified', num: filteredSummary.numEmissionAmountVerified},
+                {name: 'numHasActiveProgram', num: filteredSummary.numHasActiveProgram},
+                {name: 'numHasNetZeroGoal', num: filteredSummary.numHasNetZeroGoal},
+                {name: 'numHasProtocol', num: filteredSummary.numHasProtocol},
+                {name: 'numHasEmissionTargetVerified', num: filteredSummary.numHasEmissionTargetVerified},
+                {name: 'numHasTransitionPlan', num: filteredSummary.numHasTransitionPlan},
+                {name: 'numHasLeadershipIncentive', num: filteredSummary.numHasLeadershipIncentive},
+                {name: 'numHasSupplierEngagement', num: filteredSummary.numHasSupplierEngagement},
+                {name: 'numHasValueChainEngagement', num: filteredSummary.numHasValueChainEngagement},
+            ]);
+            setNumReport(filteredSummary.numReport);
+            setYear(currentYear);
+        }
+        if (event.target.name === "industry") {
+            var currentIndustry = event.target.value;
+            const filteredSummary = summaryData.filter((summary) => {
+                return (summary.year.toString() === year && summary.industry === currentIndustry);
+            })[0];
+            console.log(filteredSummary);
+            console.log(year);
+            console.log(currentIndustry);
+            setChartData([
+                {name: 'numEmissionAmountVerified', num: filteredSummary.numEmissionAmountVerified},
+                {name: 'numHasActiveProgram', num: filteredSummary.numHasActiveProgram},
+                {name: 'numHasNetZeroGoal', num: filteredSummary.numHasNetZeroGoal},
+                {name: 'numHasProtocol', num: filteredSummary.numHasProtocol},
+                {name: 'numHasEmissionTargetVerified', num: filteredSummary.numHasEmissionTargetVerified},
+                {name: 'numHasTransitionPlan', num: filteredSummary.numHasTransitionPlan},
+                {name: 'numHasLeadershipIncentive', num: filteredSummary.numHasLeadershipIncentive},
+                {name: 'numHasSupplierEngagement', num: filteredSummary.numHasSupplierEngagement},
+                {name: 'numHasValueChainEngagement', num: filteredSummary.numHasValueChainEngagement},
+            ]);
+            setNumReport(filteredSummary.numReport);
+            setIndustry(event.target.value);
+        }
+    };
+
+    
+
+    return (
+        <div>
+        <div style={{ marginBottom: "10px", }}>
+        <label>Pick a year:</label>
+        <select name="year" onChange={handleChange}>
+        {yearList.map((item, index) => (
+            <option value={item}>{item}</option>
+        ))}
+        </select>
+
+        <label>Pick an Industry:</label>
+        <select name="industry" onChange={handleChange}>
+        {industryList.map((item, index) => (
+            <option value={item}>{item}</option>
+        ))}
+        </select>
+
+        </div>
+        <p>Total number of reports: {numReport}</p>
+        <BarChart width={1500} height={600} data={chartData}>
+            <Bar dataKey="num" fill="green" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </BarChart>
+        
+        </div>
+    );
+ }
